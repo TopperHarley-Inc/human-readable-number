1012398d0cd4b85e8392e0edc275e95901f690ac
Topper <serhii.hruntkovskii@gmail.com>
2023-07-29 17:21:49 +0300

    fix: errors fixed

--- src/index.js
+++ src/index.js
@@ -1,30 +1,39 @@
 module.exports = function toReadable (number) {
-    const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    const teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    const tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-    const groups = ['', 'thousand', 'million', 'billion', 'trillion'];
+  const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-    function convertToWords(n) {
-      if (n === 0) return '';
-      if (n < 10) return units[n];
-      if (n < 20) return teens[n - 11];
-      if (n < 100) return tens[Math.floor(n / 10)] + ' ' + units[n % 10];
-      if (n < 1000) return units[Math.floor(n / 100)] + ' hundred ' + convertToWords(n % 100);
-      for (let i = 1; i < groups.length; i++) {
-        const base = 1000 ** i;
-        if (n < base ** 2) {
-          return convertToWords(Math.floor(n / base)) + ' ' + groups[i] + ' ' + convertToWords(n % base);
-        }
+  if (typeof number !== 'number' || number < 0 || number >= 1000) {
+    throw new Error('Invalid input. The number must be a positive three-digit integer.');
+  }
+
+  if (number === 0) return 'zero';
+  const nStringLength = number.toString().length;
+  if (nStringLength === 1) return units[number];
+
+  const hundredsDigit = Math.floor(number / 100);
+  const tensDigit = Math.floor((number % 100) / 10);
+  const unitsDigit = number % 10;
+
+  let words = '';
+
+  if (hundredsDigit > 0) {
+    words += units[hundredsDigit] + ' hundred';
+    if ((tensDigit > 0 || unitsDigit > 0) && tensDigit !== 0) {
+      words += ' ';
+    }
+  }
+
+  if (tensDigit > 0 || unitsDigit > 0) {
+    if (tensDigit === 1 && unitsDigit > 0) {
+      words += teens[unitsDigit - 1];
+    } else {
+      words += tens[tensDigit];
+      if (unitsDigit > 0) {
+        words += (tensDigit !== 0 ? ' ' : ' ') + units[unitsDigit];
       }
     }
+  }
 
-    if (number === 0) return 'zero';
-    if (number < 0) return 'minus ' + numberToWords(-number);
-
-    const numberString = number.toLocaleString('en-US');
-    const parts = numberString.split('.');
-    const wholePart = parseInt(parts[0], 10);
-    const fractionalPart = parts[1] ? ' point ' + parts[1] : '';
-
-    return convertToWords(wholePart) + fractionalPart;
+  return words;
 }
